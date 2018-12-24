3199fdeec9aa98a05592df1ca0671d672d8610cb
YinZhh <1335263679@qq.com>
2018-12-24 16:10:35 +0800

    注释添加

--- src/main/java/com/zhh/SpringBootAngularApplication.java
+++ src/main/java/com/zhh/SpringBootAngularApplication.java
@@ -3,9 +3,20 @@ package com.zhh;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author yin.zhh
+ * spring boot 启动
+ */
 @SpringBootApplication
 public class SpringBootAngularApplication {
 
+    /**
+     * @description This's Method description
+     * @author zhh_yin
+     * @date 2018/12/22 17:26
+     * @param args args
+     * @return void
+     */
     public static void main(String[] args) {
         SpringApplication.run(SpringBootAngularApplication.class, args);
     }
