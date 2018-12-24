5e9a4560cc32b92a77040f0422ea827b8ccbdaa1
YinZhh <1335263679@qq.com>
2018-12-24 17:23:41 +0800

    .

--- src/main/java/com/zhh/util/collection/CollectionUtils.java
+++ src/main/java/com/zhh/util/collection/CollectionUtils.java
@@ -48,41 +48,41 @@ public class CollectionUtils {
         return false;
     }
 
-    private static void sort(int[] a, int low, int high) {
+    private static void sort(int[] arr, int low, int high) {
         int start = low;
         int end = high;
-        int key = a[low];
+        int key = arr[low];
         while (end > start) {
             /*从后往前比较*/
-            while (end > start && a[end] >= key) {
+            while (end > start && arr[end] >= key) {
                 /*如果没有比关键值小的，比较下一个，直到有比关键值小的交换位置，然后又从前往后比较*/
                 end--;
             }
-            if (a[end] <= key) {
-                int temp = a[end];
-                a[end] = a[start];
-                a[start] = temp;
+            if (arr[end] <= key) {
+                int temp = arr[end];
+                arr[end] = arr[start];
+                arr[start] = temp;
             }
             /*从前往后比较*/
-            while (end > start && a[start] <= key) {
+            while (end > start && arr[start] <= key) {
                 /*如果没有比关键值大的，比较下一个，直到有比关键值大的交换位置*/
                 start++;
             }
-            if (a[start] >= key) {
-                int temp = a[start];
-                a[start] = a[end];
-                a[end] = temp;
+            if (arr[start] >= key) {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
             }
             /*此时第一次循环比较结束，关键值的位置已经确定了。左边的值都比关键值小，右边的值都比关键值大，但是两边的顺序还有可能是不一样的，进行下面的递归调用*/
         }
         /*递归*/
         if (start > low) {
             /*左边序列。第一个索引位置到关键值索引-1*/
-            sort(a, low, start - 1);
+            sort(arr, low, start - 1);
         }
         if (end < high) {
             /*右边序列。从关键值索引+1到最后一个*/
-            sort(a, end + 1, high);
+            sort(arr, end + 1, high);
         }
     }
 
