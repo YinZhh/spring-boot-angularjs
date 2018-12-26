60086d6af113925c479734f41aba2dedf04c03cc
YinZhh <1335263679@qq.com>
2018-12-26 14:05:33 +0800

    加载static下所有静态资源

---
+++ src/main/java/com/zhh/view/WebAppConfigurer.java
@@ -0,0 +1,54 @@
+package com.zhh.view;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author zhh.yin
+ * @version v.1.0.0
+ * @description This's class description
+ * @date 2018/12/26 11:29
+ */
+@Configuration
+public class WebAppConfigurer implements WebMvcConfigurer {
+
+    /***
+     * @description This's Method description 指定静态文件扫描路径
+     * @author zhh_yin
+     * @date 2018/12/26 14:00
+     * @param registry org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+     * @return void
+     */
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+    }
+
+    @Bean
+    public HandlerInterceptor getLoginInterceptor() {
+        //return new LoginInterceptor();
+        return null;
+    }
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+       // registry.addInterceptor(getLoginInterceptor())
+       //         .addPathPatterns("/**")
+       //         .excludePathPatterns("/error")
+       //         .excludePathPatterns("/static/*");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // registry.addMapping("/**")//设置允许跨域的路径
+        //         .allowedOrigins("*")//设置允许跨域请求的域名
+        //         .allowCredentials(true)//是否允许证书 不再默认开启
+        //         .allowedMethods("GET", "POST", "PUT", "DELETE")//设置允许的方法
+        //         .maxAge(3600);//跨域允许时间
+    }
+}
